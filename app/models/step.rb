6c49fcd549254aefebf7938536e275d139c500a8
Brian Yingling <brian_yingling@yahoo.com>
2013-04-15 21:10:47 -0400

    added geocoder to step model

--- app/models/step.rb
+++ app/models/step.rb
@@ -2,4 +2,15 @@ class Step < ActiveRecord::Base
   attr_accessible :name, :description, :address, :lat, :lng, :audiofile, :position, :tour_id
   belongs_to :tour
   has_many :statuses
+  before_save :get_coords
+
+
+  private
+  def get_coords
+    result = Geocoder.search(self.address).first
+    if result.present?
+      self.lat = result.latitude
+      self.lng = result.longitude
+    end
+  end
 end
