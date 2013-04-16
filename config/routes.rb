200f6dfac63e292ae33665bd24baae32ce268855
Brian Yingling <brian_yingling@yahoo.com>
2013-04-16 07:27:05 -0400

    added tour show page

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Tourmii::Application.routes.draw do
   root :to=>'home#index'
-  resources :users, :tours
+  resources :users, :tours, :steps
 
   get '/search/new'   => 'searches#new'
   get 'search/query'  => 'searches#query'
