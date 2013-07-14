b219511d82bff83ad9ff2f04d86cba6c0132478c
Brian Yingling <brian_yingling@yahoo.com>
2013-07-14 19:14:45 -0400

    styled css menus

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,22 +1,37 @@
 class HomeController < ApplicationController
   def index
-    # url = "http://api.flickr.com/services/rest/?method=flickr.interestingness.getList"
+    @response = query_flickr("landscapes")
+    @photo = choose_photo(@response)
+    @photo_url = build_flickr_photo_url(@photo)
+    @image_title = @photo['title']
+  end
+
+  def search
+  end
+
+  # Retrieves a json object of photos from Flickr
+  def query_flickr(keyword)
     url = "http://api.flickr.com/services/rest/?method=flickr.photos.search"
-    url += "&text=landscapes"
+    url += "&text=#{keyword}"
     url += "&api_key=" + ENV['FLICKR_TOURMII_API_KEY']
     url += "&format=json&nojsoncallback=1"
     url += "&extras=url_b"
-    @response = HTTParty.get(url)
-    num = 1 + rand(@response['photos']['photo'].size)
-    # http://farm{farm-id}.staticflickr.com/{server-id}/{id}_{o-secret}_o.(jpg|gif|png)
-    farm_id = @response['photos']['photo'][num]['farm']
-    server_id = @response['photos']['photo'][num]['server']
-    id = @response['photos']['photo'][num]['id']
-    o_secret = @response['photos']['photo'][num]['secret']
-    @photo_url = "http://farm#{farm_id}.staticflickr.com/#{server_id}/#{id}_#{o_secret}_b.jpg"
-    @image_title = @response['photos']['photo'][num]['title']
+    HTTParty.get(url)
+    end
 
+  # receives a json object from flickr and returns a url to flickr to retrieve it.
+  def build_flickr_photo_url(photo)
+    # http://farm{farm-id}.staticflickr.com/{server-id}/{id}_{o-secret}_o.(jpg|gif|png)
+    farm_id = photo['farm']
+    server_id = photo['server']
+    id = photo['id']
+    o_secret = photo['secret']
+    "http://farm#{farm_id}.staticflickr.com/#{server_id}/#{id}_#{o_secret}_b.jpg"
   end
-  def search
+
+  # randomly chooses a photo from the photos json obj returned from Flickr
+  def choose_photo(photos)
+    num = 1 + rand(photos['photos']['photo'].size)
+    photos['photos']['photo'][num]
   end
 end
