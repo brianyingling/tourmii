351deb150af43512653bb315deb8ae73faf22557
Brian Yingling <brian_yingling@yahoo.com>
2014-05-19 16:50:59 -0400

    added route for cors_preflight_check

--- config/routes.rb
+++ config/routes.rb
@@ -7,11 +7,13 @@ Tourmii::Application.routes.draw do
     end
   end
 
-
   get '/home'         => 'home#index'
   get '/search/new'   => 'searches#new'
   get 'search/query'  => 'searches#query'
   get '/login'        => 'session#new'
   post '/login'       => 'session#create'
   delete '/login'     => 'session#destroy'
+
+  match "*all" => "application#cors_preflight_check", :constraints => {:method => "OPTIONS"}
+
 end
